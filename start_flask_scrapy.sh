a5186d5ebad0d2d4d05ab49693c3946ad8b5d8c7
Feiyu <chenfeiyu0402@gmail.com>
2018-06-07 00:38:28 +0100

    fixed bash version

--- start_flask_scrapy.sh
+++ start_flask_scrapy.sh
@@ -3,4 +3,4 @@
 source ./venv/bin/activate
 echo "Starting flask scrapy with nice and low i/o priority"
 #ionice keep the process at low enough disk and CPU priority
-nohup python2.7 flask_scrapy_manage.py runserver >/dev/null 2>&1 &
+nohup python3.6 flask_scrapy_manage.py runserver >/dev/null 2>&1 &
